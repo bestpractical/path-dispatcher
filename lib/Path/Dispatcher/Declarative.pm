97c353f70aa37fa794915c79833df97912e3fc9d
sartak <sartak@e417ac7c-1bcc-0310-8ffa-8f5827389a85>
2008-07-30 19:39:21 +0000

    Figure out how sugar will be distributed

--- lib/Path/Dispatcher/Declarative.pm
+++ lib/Path/Dispatcher/Declarative.pm
@@ -3,14 +3,13 @@ package Path::Dispatcher::Declarative;
 use strict;
 use warnings;
 use Sub::Exporter;
+use Path::Dispatcher;
 
 my $exporter = Sub::Exporter::build_exporter({
-    exports => {
-    },
+    into_level => 1,
     groups => {
-        default => [':all'],
+        default => \&build_sugar,
     },
-
 });
 
 sub import {
@@ -18,13 +17,25 @@ sub import {
     my $pkg  = caller;
     my @args = grep { !/^-[Bb]ase/ } @_;
 
-    # they must have specified '-base' if there are no args
+    # they must have specified '-base' if there are a different number of args
     if (@args != @_) {
         no strict 'refs';
-        push @{ $pkg . '::ISA' }, $self
+        push @{ $pkg . '::ISA' }, $self;
     }
 
     $exporter->($self, @args);
+}
+
+sub build_sugar {
+    my ($class, $group, $arg) = @_;
+
+    my $dispatcher = Path::Dispatcher->new;
+
+    return {
+        dispatcher => sub { $dispatcher },
+        dispatch   => sub { $dispatcher->dispatch(@_) },
+        run        => sub { $dispatcher->run(@_) },
+    };
 }
 
 1;
--- t/100-declarative.t
+++ t/100-declarative.t
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use Test::More tests => 2;
+use Test::More tests => 4;
 
 do {
     package MyApp::Dispatcher;
@@ -9,6 +9,7 @@ do {
 };
 
 ok(MyApp::Dispatcher->isa('Path::Dispatcher::Declarative'), "use Path::Dispatcher::Declarative -base sets up ISA");
+can_ok('MyApp::Dispatcher', qw/dispatcher dispatch run/);
 
 do {
     package MyApp::Dispatcher::NoBase;
@@ -16,4 +17,5 @@ do {
 };
 
 ok(!MyApp::Dispatcher::NoBase->isa('Path::Dispatcher::Declarative'), "use Path::Dispatcher::Declarative without -base does not set up ISA");
+can_ok('MyApp::Dispatcher::NoBase', qw/dispatcher dispatch run/);
 
