4f3c0f413a7118970c5d957136dfc04225d5dced
sartak <sartak@e417ac7c-1bcc-0310-8ffa-8f5827389a85>
2008-08-13 09:36:55 +0000

    Simplest implementation of tokens rule

--- lib/Path/Dispatcher/Rule/Tokens.pm
+++ lib/Path/Dispatcher/Rule/Tokens.pm
@@ -4,14 +4,32 @@ use Moose;
 extends 'Path::Dispatcher::Rule';
 
 has tokens => (
-    is       => 'ro',
-    isa      => 'ArrayRef[ArrayRef[Str|RegexpRef]]',
-    required => 1,
+    is         => 'ro',
+    isa        => 'ArrayRef[Str]',
+    auto_deref => 1,
+    required   => 1,
+);
+
+has splitter => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => ' ',
 );
 
 sub _match {
     my $self = shift;
     my $path = shift;
+
+    my @tokens = split $self->splitter, $path;
+
+    for my $expected ($self->tokens) {
+        my $got = shift @tokens;
+
+        return if $got ne $expected;
+    }
+
+    return if @tokens;
+    return 1;
 }
 
 __PACKAGE__->meta->make_immutable;
