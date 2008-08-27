028a5fc0c38356fa26d785da1bea9da3caa6b967
sartak <sartak@e417ac7c-1bcc-0310-8ffa-8f5827389a85>
2008-08-27 15:05:44 +0000

    dist doc

--- lib/Path/Dispatcher.pm
+++ lib/Path/Dispatcher.pm
@@ -2,6 +2,8 @@
 package Path::Dispatcher;
 use Moose;
 
+our $VERSION = '0.01';
+
 use Path::Dispatcher::Stage;
 use Path::Dispatcher::Rule;
 use Path::Dispatcher::Dispatch;
@@ -140,4 +142,39 @@ __PACKAGE__->meta->make_immutable;
 no Moose;
 
 1;
+
+__END__
+
+=head1 NAME
+
+Path::Dispatcher - flexible dispatch
+
+=head1 DESCRIPTION
+
+We really like L<Jifty::Dispatcher> and wanted to use it for the command line.
+
+More documentation coming later, there's a lot here..
+
+=head1 AUTHOR
+
+Shawn M Moore, C<< <sartak at bestpractical.com> >>
+
+=head1 BUGS
+
+C<after> substages are not yet run properly when primary stage is run.
+
+The order matches when a super dispatch is added B<will> change.
+
+Please report any bugs or feature requests to
+C<bug-path-dispatcher at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Path-Dispatcher>.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Best Practical Solutions.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
 
