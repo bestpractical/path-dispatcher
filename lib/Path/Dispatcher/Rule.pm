f507c2c79618eda86b72c71fd5bce50a4d159a33
sartak <sartak@e417ac7c-1bcc-0310-8ffa-8f5827389a85>
2008-10-19 13:44:29 +0000

    Doc for Path::Dispatcher::Rule

--- lib/Path/Dispatcher/Rule.pm
+++ lib/Path/Dispatcher/Rule.pm
@@ -73,3 +73,61 @@ require Path::Dispatcher::Rule::Under;
 
 1;
 
+__END__
+
+=head1 NAME
+
+Path::Dispatcher::Rule - predicate and codeblock
+
+=head1 SYNOPSIS
+
+    my $rule = Path::Dispatcher::Rule::Regex->new(
+        regex => qr/^quit/,
+        block => sub { die "Program terminated by user.\n" },
+    );
+
+    $rule->match("die"); # undef, because "die" !~ /^quit/
+
+    my $match = $rule->match("quit"); # creates a Path::Dispatcher::Match
+
+    $rule->run; # exits the program
+
+=head1 DESCRIPTION
+
+A rule has a predicate and an optional codeblock. Rules can be matched (which
+checks the predicate against the path) and they can be ran (which invokes the
+codeblock).
+
+This class is not meant to be instantiated directly, because there is no
+predicate matching function. Instead use one of the subclasses such as
+L<Path::Dispatcher::Rule::Tokens>.
+
+=head1 ATTRIBUTES
+
+=head2 block
+
+An optional block of code to be run. Please use the C<run> method instead of
+invoking this attribute directly.
+
+=head2 prefix
+
+A boolean indicating whether this rule can match a prefix of a path. If false,
+then the predicate must match the entire path. One use-case is that you may
+want a catch-all rule that matches anything beginning with the token C<ticket>.
+The unmatched, latter part of the path will be available in the match object.
+
+=head1 METHODS
+
+=head2 match path -> match
+
+Takes a path and returns a L<Path::Dispatcher::Match> object if it matched the
+predicate, otherwise C<undef>. The match object contains information about the
+match, such as the results (e.g. for regex, a list of the captured variables),
+the C<leftover> path if C<prefix> matching was used, etc.
+
+=head2 run
+
+Runs the rule's codeblock. If none is present, it throws an exception.
+
+=cut
+
