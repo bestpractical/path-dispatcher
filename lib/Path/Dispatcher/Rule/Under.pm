8b18fb0baa3a89cc5a49c3e4a5dcb83734344da5
sartak <sartak@e417ac7c-1bcc-0310-8ffa-8f5827389a85>
2008-10-20 12:04:39 +0000

    Rule::Under doc

--- lib/Path/Dispatcher/Rule/Under.pm
+++ lib/Path/Dispatcher/Rule/Under.pm
@@ -41,3 +41,55 @@ sub match {
 
 1;
 
+__END__
+
+=head1 NAME
+
+Path::Dispatcher::Rule::Under - rules under a predicate
+
+=head1 SYNOPSIS
+
+    my $ticket = Path::Dispatcher::Rule::Tokens->new(
+        tokens => [ 'ticket' ],
+        prefix => 1,
+    );
+
+    my $create = Path::Dispatcher::Rule::Tokens->new(
+        tokens => [ 'create' ],
+        block  => sub { create_ticket() },
+    );
+
+    my $delete = Path::Dispatcher::Rule::Tokens->new(
+        tokens => [ 'delete', qr/^\d+$/ ],
+        block  => sub { delete_ticket($2) },
+    );
+
+    my $rule = Path::Dispatcher::Rule::Under->new(
+        predicate => $ticket,
+        rules     => [ $create, $delete ],
+    );
+
+    $rule->match("ticket create");
+    $rule->match("ticket delete 3");
+
+=head1 DESCRIPTION
+
+Rules of this class have two-phase matching: if the predicate is matched, then
+the contained rules are matched. The benefit of this is less repetition of the
+predicate, both in terms of code and in matching it.
+
+=head1 ATTRIBUTES
+
+=head2 predicate
+
+A rule (which I<must> match prefixes) whose match determines whether the
+contained rules are considered. The leftover path of the predicate is used
+as the path for the contained rules.
+
+=head2 rules
+
+A list of rules that will be try to be matched only if the predicate is
+matched.
+
+=cut
+
