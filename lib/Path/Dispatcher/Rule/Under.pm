a92dda9c984cb74f9e6708f5d5524e9f4d3508f9
sartak <sartak@e417ac7c-1bcc-0310-8ffa-8f5827389a85>
2008-10-20 12:05:49 +0000

    Make under rules immutable and remove their sugar

--- lib/Path/Dispatcher/Rule/Under.pm
+++ lib/Path/Dispatcher/Rule/Under.pm
@@ -39,6 +39,10 @@ sub match {
     return grep { defined } map { $_->match($suffix) } $self->rules;
 }
 
+__PACKAGE__->meta->make_immutable;
+no Moose;
+no Moose::Util::TypeConstraints;
+
 1;
 
 __END__
