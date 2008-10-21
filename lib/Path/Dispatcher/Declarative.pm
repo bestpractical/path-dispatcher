930c4d8549d58b4be10761cfade4af547492ea9b
sartak <sartak@e417ac7c-1bcc-0310-8ffa-8f5827389a85>
2008-10-21 19:45:33 +0000

    Rename "run" to "redispatch"

--- lib/Path/Dispatcher/Declarative.pm
+++ lib/Path/Dispatcher/Declarative.pm
@@ -59,7 +59,7 @@ sub build_sugar {
 
             $OUTERMOST_DISPATCHER->dispatch(@_);
         },
-        run => sub {
+        redispatch => sub {
             # if caller is $into, then this function is being used as sugar
             # otherwise, it's probably a method call, so discard the invocant
             shift if caller ne $into;
@@ -208,7 +208,7 @@ L<Path::Dispatcher::Dispatch> object. Acts as a keyword within the same
 package; otherwise as a method (since these declarative dispatchers are
 supposed to be used by other packages).
 
-=head2 run path, args
+=head2 redispatch path, args
 
 Performs a dispatch then invokes the L<Path::Dispatcher::Dispatch/run> method
 on it.
