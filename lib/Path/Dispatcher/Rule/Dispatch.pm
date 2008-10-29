2eefa3667ee456e719a7637c900c4c473a20efc2
sartak <sartak@e417ac7c-1bcc-0310-8ffa-8f5827389a85>
2008-10-29 23:34:59 +0000

    Skeleton of the Rule::Dispatch doc

--- lib/Path/Dispatcher/Rule/Dispatch.pm
+++ lib/Path/Dispatcher/Rule/Dispatch.pm
@@ -23,3 +23,43 @@ no Moose::Util::TypeConstraints;
 
 1;
 
+__END__
+
+=head1 NAME
+
+Path::Dispatcher::Rule::Dispatch - redispatch
+
+=head1 SYNOPSIS
+
+    my $dispatcher = Path::Dispatcher->new(
+        rules => [
+            Path::Dispatcher::Rule::Tokens->new(
+                tokens => [  ],
+                block  => sub {  },
+            ),
+            Path::Dispatcher::Rule::Tokens->new(
+                tokens => [  ],
+                block  => sub {  },
+            ),
+        ],
+    );
+
+    my $rule = Path::Dispatcher::Rule::Dispatch->new(
+        dispatcher => $dispatcher,
+    );
+
+    $rule->run("");
+
+=head1 DESCRIPTION
+
+Rules of this class use another dispatcher to match the path.
+
+=head1 ATTRIBUTES
+
+=head2 dispatcher
+
+A L<Path::Dispatcher> object. Its matches will be returned by matching this
+rule.
+
+=cut
+
