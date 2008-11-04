f282fd148b78ab0038b21188f7d9654704804f09
sartak <sartak@e417ac7c-1bcc-0310-8ffa-8f5827389a85>
2008-11-04 22:35:00 +0000

    Include the regex in the (unlikely) error just in case

--- lib/Path/Dispatcher/Match.pm
+++ lib/Path/Dispatcher/Match.pm
@@ -72,7 +72,7 @@ sub run_with_number_vars {
     # we need to do the match anyway, because we have to clear the number vars
     ($str, $re) = ("x", "x") if length($str) == 0;
     $str =~ $re
-        or die "Unable to match '$str' against a copy of itself!";
+        or die "Unable to match '$str' against a copy of itself ($re)!";
 
     $code->();
 }
