bc6fc4daafe207420667e44003ce8b3f828b87bc
sartak <sartak@e417ac7c-1bcc-0310-8ffa-8f5827389a85>
2008-11-19 00:17:08 +0000

    Support for single string arg in new: Path::Dispatcher::Path->new("/my/path")

--- lib/Path/Dispatcher/Path.pm
+++ lib/Path/Dispatcher/Path.pm
@@ -14,6 +14,18 @@ has metadata => (
     predicate => 'has_metadata',
 );
 
+# allow Path::Dispatcher::Path->new($path)
+around BUILDARGS => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    if (@_ == 1 && !ref($_[0])) {
+        unshift @_, 'path';
+    }
+
+    $self->$orig(@_);
+};
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
