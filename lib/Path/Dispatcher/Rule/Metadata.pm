8ff35e2ec1475d563f3126f256f21377f50dcb89
sartak <sartak@e417ac7c-1bcc-0310-8ffa-8f5827389a85>
2008-11-19 01:00:28 +0000

    Make metadata only match a single metadata value

--- lib/Path/Dispatcher/Rule/Metadata.pm
+++ lib/Path/Dispatcher/Rule/Metadata.pm
@@ -4,39 +4,26 @@ use Moose;
 use MooseX::AttributeHelpers;
 extends 'Path::Dispatcher::Rule';
 
-has match_metadata => (
-    metaclass => 'Collection::Hash',
-    is        => 'rw',
-    isa       => 'HashRef',
-    required  => 1,
-    provides  => {
-        keys => 'metadata_keys',
-        get  => 'metadata',
-    },
+has name => (
+    is       => 'rw',
+    isa      => 'Str',
+    required => 1,
+);
+
+has value => (
+    is       => 'rw',
+    isa      => 'Str',
+    required => 1,
 );
 
 sub _match {
     my $self = shift;
     my $path = shift;
+    my $got = $path->get_metadata($self->name);
 
-    my $path_metadata = $path->metadata;
-
-    for my $key ($self->metadata_keys) {
-        return 0 if !exists($path_metadata->{$key});
-
-        $self->_match_metadatum($path_metadata->{$key}, $self->metadata($key))
-            or return 0;
-    }
+    return 0 if $self->value ne $got;
 
     return 1, $path->path;
-}
-
-sub _match_metadatum {
-    my $self     = shift;
-    my $got      = shift;
-    my $expected = shift;
-
-    return $got eq $expected;
 }
 
 1;
