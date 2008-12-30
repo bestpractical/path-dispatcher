bdd28f3696130b5b80746ac79cfef8bac2cc40a1
sartak <sartak@e417ac7c-1bcc-0310-8ffa-8f5827389a85>
2008-12-30 19:21:11 +0000

    Some doc for Path::Dispatcher::Path

--- lib/Path/Dispatcher/Path.pm
+++ lib/Path/Dispatcher/Path.pm
@@ -37,3 +37,35 @@ no Moose;
 
 1;
 
+__END__
+
+=head1 NAME
+
+Path::Dispatcher::Path - path and some optional metadata
+
+=head1 SYNOPSIS
+
+    my $path = Path::Dispatcher::Path->new(
+        path     => "/REST/Ticket/1",
+        metadata => {
+            http_method => "DELETE",
+        },
+    );
+
+    $path->path;                        # /REST/Ticket/1
+    $path->get_metadata("http_method"); # DELETE
+
+=head1 ATTRIBUTES
+
+=head2 path
+
+A string representing the path. C<Path::Dispatcher::Path> is basically a boxed
+string. :)
+
+=head2 metadata
+
+A hash representing arbitrary metadata. The L<Path::Dispatcher::Rule::Metadata>
+rule is designed to match against members of this hash.
+
+=cut
+
