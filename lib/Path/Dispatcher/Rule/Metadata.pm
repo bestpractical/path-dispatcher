7852bd21ba06b07ba499aeb7c443bf0c712fb5e2
Shawn M Moore <sartak@gmail.com>
2009-02-23 21:58:38 -0500

    Doc for Metadata rule

--- lib/Path/Dispatcher/Rule/Metadata.pm
+++ lib/Path/Dispatcher/Rule/Metadata.pm
@@ -38,3 +38,41 @@ no Any::Moose;
 
 1;
 
+__END__
+
+=head1 NAME
+
+Path::Dispatcher::Rule::Metadata - match path's metadata
+
+=head1 SYNOPSIS
+
+    my $path = Path::Dispatcher::Path->new(
+        path => '/REST/Ticket'
+        metadata => {
+            http_method => 'POST',
+        },
+    );
+
+    my $rule = Path::Dispatcher::Rule::Metadata->new(
+        field   => 'http_method',
+        matcher => Path::Dispatcher::Rule::Eq->new(string => 'POST'),
+    );
+
+    $rule->run($path);
+
+=head1 DESCRIPTION
+
+Rules of this class match the metadata portion of a path.
+
+=head1 ATTRIBUTES
+
+=head2 field
+
+The metadata field/key name.
+
+=head2 matcher
+
+A L<Path::Dispatcher::Rule> object for matching against the value of the field.
+
+=cut
+
